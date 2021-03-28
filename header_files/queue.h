0cd6fb32c5c79d1107d4d5721dfc87910bae5b4b
Daniel.Brodsky <brod.daniel@gmail.com>
2021-03-29 02:00:30 +0300

    queue-task

---
+++ header_files/queue.h
@@ -0,0 +1,52 @@
+/****************************** File Header ***********************************\
+* File Name: queue.h
+* Author: Daniel Brodsky
+* Description: API of a Queue (FIFO) API based on a Singly Linked List wrap. 
+* Date: 29/03/2021
+* Version: 1.0
+\******************************************************************************/
+
+#ifndef	__QUEUE_H__
+#define	__QUEUE_H__
+
+typedef queue queue_ty;
+
+/* Creates an empty queue and returns pointer to handler struct */
+/* returns NULL on failure*/
+/* Complexity: O(1) */
+queue_ty *QueueCreate(void);
+
+/* Frees entire queue */
+/* Complexity: O(n) */
+void QueueDestroy(queue_ty *queue);
+
+/* Adds a new node after rear and moves rear to the next node. */
+/* Undefined behaviour if iter is queue_END */
+/* Complexity: O(1) */
+queue_iter_ty EnQueue(queue_iter_ty iter, void *data);
+
+/* Removes the front node and moves front to the next node. */
+/* Undefined behaviour if the queue is empty */
+/* Complexity: O(1) */
+queue_iter_ty DeQueue(queue_iter_ty iter);
+
+/* Returns TRUE if queue is empty or FALSE otherwise */
+/* Complexity: O(1) */
+boolean_ty QueueIsEmpty(const queue_ty *queue);
+
+/* Returns number of elements in the queue */
+/* Complexity: O(n) */
+size_t QueueGetSize(const queue_ty *queue);
+
+/* Returns the data of the iterator */
+/* Undefined behaviour if iter is queue_END */
+/* Complexity: O(1) */
+void *QueuePeek(const queue_iter_ty iter);
+
+/* Insters the element after the iterator, returns iterator to the new node */
+/* On failure returns the same iterator */
+/* Undefined behaviour if iter is queue_END */
+/* Complexity: O(1) */
+queue_iter_ty QueueAppend(queue_iter_ty iter, void *data);
+
+#endif	/* __QUEUE_H__ */
---
+++ source_files/queue.c
@@ -0,0 +1,70 @@
+/**********************************FILE-HEADER*********************************\
+* File: queue.c						 		  								
+* Author: Daniel Brodsky					  								
+* Date: 29/03/2021							   								
+* Version: Beta					   								
+* Reviewer: Ariel							   								
+* Description: Queue implementation wrapped by Singly Linked List's API.			 
+\******************************************************************************/
+
+/******************************** Inclusions **********************************/
+
+#include <assert.h> /* assert */
+#include <stddef.h>	/* size_t */
+#include <stdio.h> /* fprintf */
+#include <stdlib.h> /* malloc, free */
+
+#include "slist.h" /* singly linked list API wrapper */
+#include "queue.h"
+
+/******************************* Global Definitions ***************************/
+
+struct queue
+{
+	slist_ty *list;
+	slist_iter_ty front;	/* stores the first node */
+	slist_iter_ty rear;		/* stores the last node */
+};
+
+/************************Functions Implementations*****************************/
+/******************************************************************************/
+
+queue_ty *QueueCreate()
+{
+	queue_ty *new_queue = (queue_ty *)malloc(sizeof(queue_ty));
+	if (NULL == new_queue)
+	{
+		fprintf(stderr, "Failed to allocate memory\n");
+		return(NULL);
+	}
+	
+	new_queue->list = SlistCreate();
+	if (NULL == new_queue->list)
+	{
+		free(new_queue);
+		fprintf(stderr, "Failed to allocate memory\n");
+		return(NULL);
+	}
+	
+	new_queue->front = new_queue->list->head;
+	if (NULL == new_queue->front)
+	{
+		free(new_queue->list);
+		free(new_queue);
+		fprintf(stderr, "Failed to allocate memory\n");
+		return(NULL);
+	}
+	
+	new_queue->rear = new_queue->list->tail;
+	if (NULL == new_queue->rear)
+	{
+		free(new_queue->front);
+		free(new_queue->list);
+		free(new_queue);
+		fprintf(stderr, "Failed to allocate memory\n");
+		return(NULL);
+	}
+	
+	return(new_queue);
+}
+/******************************************************************************/
