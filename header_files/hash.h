6649f2ebe332303b52f0208b8ab93f8d883c7828
danielbrodi <broddanik@gmail.com>
2021-06-09 17:15:43 +0300

    new worksheet & updated api

--- header_files/hash.h
+++ header_files/hash.h
@@ -1,35 +1,55 @@
+/*********************************FILE__HEADER*********************************\
+* File:				hash.h						 		  								
+* Author:			Daniel Brodsky					  								
+* Date:				09-06-2021							   												   								
+* Description:		Hash Map API.		 
+\******************************************************************************/
 
-/*API FOR HASH TABLE*/
+#ifndef __HASH_H__
+#define __HASH_H__
 
-typedef struct item
+#include <stddef.h>		/*	size_t	*/
+
+typedef struct hash_table ht_ty;
+
+typedef struct statistics
 {
-	char *key;
-	char *value 
-}item_ty;
+	size_t longest_list;
+	size_t median_list;
+	float std;
+}statistics_ty;
 
-typedef int (*Hash_Func)(int *id);
+typedef size_t (*hash_func_ty)(const void *data, const void *param);
 
-typedef struct hash_table
-{
-	Hash_Func hash_function;
-	
-	item_ty** items;
-	
-	int size;
-}ht_ty;
+typedef int (*is_same_key_func_ty)(const void *data1, const void *data2);
 
-ht_ty *HTCreate(int size, Hash_Func hash_function);
+/*Complexity: O(1)*/
+ht_ty *HTCreate(size_t capacity, hash_func_ty hash_func, Is_Same_Key_ty is_same_func, const void *hash_param);
 
-void HTDestory(ht_ty *table);
+/*Doesn't release user's data*/
+/*Complexity: O(1)*/
+void HTDestroy(ht_ty *hash_table);
 
-int HTInsert(ht_ty *table, char *key, char *value);
+/*Complexity: O(1)*/
+int HTInsert(ht_ty *hash_table, void *data);
 
-void HTRemove(ht_ty *table, char *key)
+/*Returns the number of occupied places in the table*/
+/*Complexity: O(n)*/
+size_t HTSize(const ht_ty *hash_table);
 
-char *HTFind(ht_ty *table, char* key);
+/*Returns NULL if key not found*/
+/*Complexity: O(1)*/
+void *HTFind(ht_ty *hash_table, const void *key);
 
+/*Complexity: O(n)*/
+int HTIsEmpty(const ht_ty *hash_table);
 
+/*Note: removing non-existing data is ignored*/
+/*Complexity: O(1)*/
+void HTRemove(ht_ty *hash_table, const void *key);
 
+/*Complexity: O(n)*/ /*Phase 2*/
+statistics_ty HTGetStatistics(const ht_ty *hash_table);
+/*longest list, median list, and standard deviation as a struct*/
 
-
-
+#endif /*__HASH_H__*/
