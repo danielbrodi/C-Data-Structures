7debf5c2695948ce27e7529fed90059b13ae2ce5
danielbrodi <broddanik@gmail.com>
2021-06-17 13:06:46 +0300

    DhcpCreate, DhcpDestroy done. DhcpAllocateIp semi-done

--- source_files/dhcp.c
+++ source_files/dhcp.c
@@ -5,7 +5,7 @@
 * Code Reviewer:	
 * Pseudo Reviewers: Kobi, Rostik					   								
 * Version:			1.0   								
-* Description:		DHCP module implementation pseudocode.
+* Description:		DHCP module implementation based on a trie.
 \******************************************************************************/
 
 /* trie's nodes strucure */
@@ -18,71 +18,141 @@ typedef struct trie_node
 /* DHCP manager */
 struct dhcp
 {
-	trie_node_ty rooti;
+	trie_node_ty root;
 	address_ty subnet;
 	int num_variable_bits;
 };
+
+enum
+{
+	SUCCESS = 0,
+	NO_AVAILABLE_IPS = 1,
+	MEMORY_ALLOCATION_ERR = 2
+}
 /*****************************Pseudo-Code*************************************/
 /******************************************************************************/
 dhcp_ty *DhcpCreate(address_ty subnet, int num_variable_bits)
 {
+	dhcp_ty *new_dhcp = NULL;
+	
+	/* asserts */
+	assert(subnet);
+	assert(num_variable_bits);
+	
 	/*	allocate memory for dhcp struct and handle errors*/
-
-	/*	allocate memory and create trie, handle errors*/
-	/*	assign it to dhcp*/
+	new_dhcp = (dhcp_ty *)malloc(sizeof(dhcp_ty));
+	if (!new_dhcp)
+	{
+		return (NULL);
+	}
+	
+	/*	define the subnet as the "root" of our dhcp */
+	new_dhcp->root
 	
 	/*	assign parameters to dhcp struct memebrs*/
+	new_dhcp->subnet = subnet;
+	new_dhcp->num_variable_bits = num_variable_bits;
 	
 	/*	return pointer to dhcp*/
+	return (new_dhcp);
 }
 /******************************************************************************/
 void DhcpDestroy(dhcp_ty *dhcp)
 {
-	/*	destroy trie*/
-	/*	nullify dhcp struct members*/
-	/*	free dhcp*/
+	if (dhcp)
+	{
+		TrieDestoryIMP(dhcp->root);
+		
+		/*	nullify dhcp struct members*/
+		memset(dhcp, 0, sizeof(dhcp_ty));
+		
+		/*	free dhcp*/
+		free(dhcp);
+	}
 }
 /******************************************************************************/
 int DhcpAllocateIp(dhcp_ty *dhcp, address_ty preferred_ip)
 {
+	int ret = 0;
 	/*	asserts*/
+	assert(dhcp);
 	
-	/* if AllocteIpIMP has failed: */
-		/* Call AllocteIpIMP on the smallest group in the subnet */
+	ret = AllocteIpIMP(&dhcp->root, &preferred_ip, dhcp->num_variable_bits);
+	/* if AllocteIpIMP has failed because of a mem error: */
+	if (MEMORY_ALLOCATION_ERR == ret)
+	{
+		return (MEMORY_ALLOCATION_ERR);
+	}
+	
+	/* if AllocteIpIMP has failed because no available ip: */
+	if (NO_AVAILABLE_IPS == ret))
+	{
+		/* TODO Call AllocteIpIMP on the smallest group in the subnet */
+		ret = AllocteIpIMP()
 		/* If also this failed: */
+		if (NO_AVAILABLE_IPS == ret)
+		{
 			/* return failured */
+			return (NO_AVAILABLE_IPS);
+		}
+	}
 		
 	/*	return success*/
+	return (SUCCESS);
 }
 /*----------------------------------------------------------------------------*/
 static int AllocteIpIMP(trie_node_ty **node, address_ty *preferred_ip, int num_variable_bits)
 {
+	trie_node_ty *new_node = NULL;
 	
 	/*	if node is null:*/
-	/*		create node, handle memory errors */
-	/*		connect created node to the tree */
+	if (!node)
+	{
+		/*		create node, handle memory errors */
+		new_node = CreateNode(&node);
+		if (!new_node)
+		{
+			return (MEMORY_ALLOCATION_ERR);
+		}
+		
+		/* TODO		connect created node to the tree */
+	}
 	
 	/*	if node is full:*/
-	/*		return  failure */
-
-	/*	if num_variable_bits == 0:*/
-	/*		mark as full*/
-	/*		return succeess*/
-
+	if (node->is_full)
+	{
+		/*	return  failure	 */
+		return (NO_AVAILABLE_IPS);
+	}
+	
+	/*	if num_variable_bits == 0:	*/
+	if (!num_variable_bits)
+	{
+		/*	mark as full	*/
+		node->is_full = 1;
+		/*	return succeess	*/
+		return (SUCCESS);
+	}
 
 	/*				// side depends on preferred_ip )*/
-	/*	recursively call AllocteIpIMP with needed side (by ip) of node 
-	/*	if succeess:*/
-	/*		update children of node as full*/
-
+	/*	TODO recursively call AllocteIpIMP with needed side (by ip) of node */
+	if (SUCCESS == AllocteIpIMP()
+	{
+		/*	if succeess:*/
+		/*		update children of node as full*/
+		node->children[0]->is_full = node->children[1]->is_full = 1;
+	}
+	
 	/*	else (failure):	*/
+	else
+	{
 		/* if side was left side, which means we failed to go on left child:*/
 		/*			nullify rest of the tree*/
 		/*			call AllocteIpIMP on right subtree
 					if this call was successful:
 						update ip to be with '1' at the current level
-						return succeess
-
+						return succeess */
+	}
 		/*	return failure */
 }
 /******************************************************************************/
